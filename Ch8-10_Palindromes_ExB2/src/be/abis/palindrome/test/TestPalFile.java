f73232ef8da647ee99143a3bcacb4ab97502b0b5
Sandy Schillebeeckx <sschillebeeckx@abis.be>
2023-11-23 16:26:35 +0100

    Chapter 8-10 - Utilities, I/O and Lambdas - Palindromes

---
+++ Ch8-10_Palindromes_ExB2/src/be/abis/palindrome/test/TestPalFile.java
@@ -0,0 +1,62 @@
+package be.abis.palindrome.test;
+
+import be.abis.palindrome.exception.NotAPalindromeException;
+import be.abis.palindrome.model.Palindrome;
+import be.abis.palindrome.model.PalindromePhrase;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class TestPalFile {
+
+    public static void main(String[] args) {
+
+        ArrayList<Palindrome> palArrList = new ArrayList<>();
+
+        try (FileReader fr = new FileReader("Ch8-10_Palindromes_ExB2/src/palindromes.txt");
+             BufferedReader br = new BufferedReader(fr);
+             PrintWriter pw = new PrintWriter("Ch8-10_Palindromes_ExB2/src/palindromessorted.txt")){
+            String line=null;
+            while((line=br.readLine())!=null){
+                String trimmed = line.trim();
+                Palindrome p=null;
+                if(!trimmed.equals("") && !trimmed.equals("null")){
+                    if (trimmed.indexOf(" ")>0){
+                        p=new PalindromePhrase(trimmed);
+                    } else {
+                        p=new Palindrome(trimmed);
+                    }
+                    if (!palArrList.contains(p)) palArrList.add(p);
+                }
+
+            }
+
+        int nrCorrectPalindromes = 0;
+            int originalNrOfValues = palArrList.size();
+
+        Iterator<Palindrome> palIter = palArrList.iterator();
+
+        while (palIter.hasNext()){
+            Palindrome p = palIter.next();
+            try {
+                p.isPalindrome();
+                nrCorrectPalindromes++;
+            } catch (NotAPalindromeException npe) {
+                palIter.remove();
+                System.out.println(p + npe.getMessage());
+            }
+        }
+
+            palArrList.sort((p1,p2)->p1.getValue().compareToIgnoreCase(p2.getValue()));
+            for(Palindrome p : palArrList) {pw.println(p);}
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+

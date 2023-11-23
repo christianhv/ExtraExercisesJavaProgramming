a54eb9e27bfa8bab064668f9620e73552b9978dc
Sandy Schillebeeckx <sschillebeeckx@abis.be>
2023-11-23 16:27:22 +0100

    Chapter 8-10 - Utilities, I/O and Lambdas - Shapes

---
+++ Ch8-10_Shapes_ExC1/src/be/abis/shapes/test/Client.java
@@ -0,0 +1,46 @@
+package be.abis.shapes.test;
+
+import be.abis.shapes.exception.AreaTooBigException;
+import be.abis.shapes.model.*;
+import be.abis.shapes.util.AreaTester;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Client {
+
+	public static void main(String[] args)  {
+
+		Point p = new Point();
+		
+		List<Shape> shapeList = new ArrayList<>();
+		
+		shapeList.add(new Circle(Color.RED,p,2));
+		shapeList.add(new Rectangle(Color.BLUE,new Point(),10,2));
+		shapeList.add(new Square(Color.YELLOW,new Point(),5.6));
+		shapeList.add(new Triangle(Color.ORANGE,new Point(),5,8));
+		shapeList.add(new Trapezoid(Color.BLUE,new Point(),5,8, 3));
+		shapeList.add(new Circle(Color.BLACK,new Point(),10));
+		shapeList.add(new Triangle(Color.YELLOW,new Point(),0.5,0.8));
+
+		System.out.println("\n----------------AREATESTER----------------------");
+
+	    for (Shape s : shapeList){
+			try {
+				String result = AreaTester.checkArea(s);
+				System.out.println(result);
+			} catch (AreaTooBigException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
+		for (Shape sh: shapeList){
+			System.out.println(sh);
+			System.out.println("Small? " +AreaTester.isSmallShape(sh));
+		}
+
+
+	}
+
+}
